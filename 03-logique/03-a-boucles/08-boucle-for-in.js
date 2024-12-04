5a48b08f1ff9cd48cd0acd634ec3eec5b1ca9b79
Sonia Sabbahi <soniadevpro@gmail.com>
2024-12-04 16:41:22 +0100

    boucle for in

---
+++ 03-logique/03-a-boucles/08-boucle-for-in.js
@@ -0,0 +1,13 @@
+// 1)
+//     Affiche les indices de chaque élément d’un tableau.
+
+//     const couleurs = ['rouge', 'vert', 'bleu'];
+
+
+const couleurs = ['rouge', 'vert', 'bleu'];
+
+for ( index in couleurs) {
+    console.log(index);
+    console.log(couleurs[index]);
+    
+}
--- 04-array/01-array.js
+++ 04-array/01-array.js
@@ -38,3 +38,29 @@ a.shift()
 a.pop()
 
 console.log(a);
+
+// Crée un tableau avec cinq éléments et utilise la propriété length pour afficher le nombre d'éléments.
+
+
+let couleurs = ["rouge", "bleu", "vert", "jaune", "noir"];
+
+console.log(couleurs.length);
+
+// Crée un tableau ["pomme", "banane", "poire"] et utilise une boucle for pour afficher chaque élément.
+
+
+const fruits2 = ["pomme", "banane", "poire"]
+
+for(let i = 0; i < fruits2.length; i++) {
+    console.log(fruits2[i]);
+} 
+
+// Doubler les nombres avec map
+// Déclare un tableau [1, 2, 3, 4]. Utilise map pour créer un nouveau tableau contenant les doubles des nombres et affiche le résultat.
+
+
+const tab = [1, 2, 3, 4]
+
+const tab2 = tab.map((x) => x *2)
+
+console.log(tab2);
