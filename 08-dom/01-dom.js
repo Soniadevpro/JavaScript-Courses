2cc1297b103ff91299f8fa3f2d8f18a2dc8572c6
Sonia Sabbahi <soniadevpro@gmail.com>
2024-12-05 14:28:26 +0100

    update fichier script et cour DOM

---
+++ 08-dom/01-dom.js
@@ -0,0 +1,50 @@
+
+const number = 3;
+const number2 = 3;
+
+console.log(number + number2);
+
+
+
+
+const box = document.getElementsByClassName("boite");
+console.log(box);
+
+
+
+// <!--3 Sélectionne tous les éléments <p> et affiche le nombre total de <p> présents. -->
+
+
+const para = document.getElementsByTagName('p')
+console.log(para.length);
+
+
+// <!--4 sélectionne le premier <span> et affiche son contenu texte dans la console. -->
+
+const span = document.querySelector(".conteneur span");
+      console.log(span.textContent);
+
+
+// <!--5 sélectionne tous les <li> et affiche leur contenu un par un dans la console. -->
+
+const list = document.querySelector('li')
+console.log(list);
+
+
+// <!--6 Sélectionne le div class box  et modifie sa classe pour qu’il devienne "box active". 
+
+const boxActive = document.querySelector('box')
+
+
+
+// Affiche l'élément modifié dans la console. -->
+
+
+
+
+
+
+// <!--7 Supprime le paragraphe <p>Texte à supprimer</p> . -->
+
+
+
