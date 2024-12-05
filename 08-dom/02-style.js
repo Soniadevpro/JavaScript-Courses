52a100abf21b61bab9df4c593374c43d693c55d3
Sonia Sabbahi <soniadevpro@gmail.com>
2024-12-05 15:21:57 +0100

    style dom

--- 08-dom/02-style.js
+++ 08-dom/02-style.js
@@ -9,3 +9,20 @@ para.style.fontSize = '24px';
 
 // lorsque elements avec un s (pluriel) ceci comprend un groupe d'élément donc un tableau i lfaut donc préciser 'element' et son index [0]
 
+
+
+
+
+
+// 3 Crée un élément <div> et applique la classe card définie dans un fichier CSS. 
+
+const styleCard = document.getElementById('style')
+styleCard.classList.add('card')
+
+
+
+// <!-- 5 Crée un <div> carré en html css et applique un fond gris en js. -->
+
+
+const carre = document.querySelector('.carre')
+carre.style.backgroundColor = ('grey')
